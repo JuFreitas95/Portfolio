fe36217322fd745fa062ee5b697f7737d7ea6d97
JuFreitas95 <judrew84@gmail.com>
2025-04-11 17:36:02 -0300

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+
+
+// function cliquei() {
+   
+//     window.open("Juliana Santiago de Freitas - Desenvolvedora Front-End.pdf")
+
+// }
+
+// window.addEventListener("click, cliquei")
+
+// cliquei()
