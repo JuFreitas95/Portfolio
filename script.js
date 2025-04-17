c05eb070b366dce94d80ad1debc8bae2a4cc0c36
JuFreitas95 <judrew84@gmail.com>
2025-04-17 15:31:06 -0300

    Commit adicionado o swiperlider com 5 certificados

--- script.js
+++ script.js
@@ -12,3 +12,31 @@
 // window.addEventListener("click, cliquei")
 
 // cliquei()
+
+
+// let list = document.querySelector('.slider .list');
+// let items = document.querySelectorAll('.slider .list .item');
+// let dots = document.querySelectorAll('.slider .dots li');
+// let prev = document.getElementById('prev');
+// let next = document.getElementById('next');
+
+// let active = 0;
+// let lengthItems = items.length;
+
+// next.onclick = function() {
+// if(active + 1 > lengthItems){
+
+
+// }else {
+//     active = active +1;
+// }
+  
+
+//     reloadSlider();
+
+// }
+
+// function reloadSlider() {
+//     let checkLeft = items[active].offsetLeft;
+//     list.computedStyleMap.left = -checkLeft + 'px';
+// }
